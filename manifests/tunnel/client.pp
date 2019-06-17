43cb3db3e4b956ebfdf98be57ad0959ce141c019
Alexander Fisher <alex@linfratech.co.uk>
2019-06-17 12:22:38 +0100

    Replace `source` and `dest` parameters
    Split `source` parameter into `source_socket_file`, `source_host` and
    `source_port` parameters.  Rename `dest` parameter to `target_`
    parameters.
    
    By splitting the parameters, it's much easier to do proper input
    validation and build the socket parameters passed to the binary
    correctly.  For instance, IP addresses are supposed to be surrounded by
    square brackets but hostnames should not.
    
    See https://github.com/Tarsnap/spiped/blob/5c13832aeecdad8a655dadcf5413cc504ad99e49/libcperciva/util/sock.c#L253
    
    Replace erb with epp template.

--- manifests/tunnel/client.pp
+++ manifests/tunnel/client.pp
@@ -1,12 +1,23 @@
 define spiped::tunnel::client(
-  $source,
-  $dest,
-  $secret,
-) {
+  Variant[Sensitive[String[1]],String[1]] $secret,
+
+  Optional[Stdlib::Unixpath] $source_socket_file = undef,
+  Optional[Stdlib::Host]     $source_host = undef,
+  Optional[Stdlib::Port]     $source_port = undef,
+
+  Optional[Stdlib::Unixpath] $target_socket_file = undef,
+  Optional[Stdlib::Host]     $target_host = undef,
+  Optional[Stdlib::Port]     $target_port = undef,
+)
+{
   spiped::tunnel { $title:
-    type   => 'client',
-    source => $source,
-    dest   => $dest,
-    secret => $secret;
+    type               => 'client',
+    source_socket_file => $source_socket_file,
+    source_host        => $source_host,
+    source_port        => $source_port,
+    target_socket_file => $target_socket_file,
+    target_host        => $target_host,
+    target_port        => $target_port,
+    secret             => $secret,
   }
 }
--- spec/defines/spiped_tunnel_server_spec.rb
+++ spec/defines/spiped_tunnel_server_spec.rb
@@ -4,8 +4,9 @@ describe 'spiped::tunnel::server' do
   let(:title) { 'redis' }
   let(:params) do
     {
-      source: '0.0.0.0:1234',
-      dest:   '/var/run/redis.sock',
+      source_host: '0.0.0.0',
+      source_port: 1234,
+      target_socket_file: '/var/run/redis.sock',
       secret: 'hunter2'
     }
   end
@@ -18,8 +19,9 @@ describe 'spiped::tunnel::server' do
       it {
         is_expected.to contain_spiped__tunnel('redis').with(
           type: 'server',
-          source: '0.0.0.0:1234',
-          dest:   '/var/run/redis.sock',
+          source_host: '0.0.0.0',
+          source_port: 1234,
+          target_socket_file: '/var/run/redis.sock',
           secret: 'hunter2'
         )
       }
@@ -46,7 +48,7 @@ describe 'spiped::tunnel::server' do
           expect(content).to include('Description=spiped tunnel (redis)')
         end
         it 'ExecStart' do
-          expect(content).to include('ExecStart=/usr/bin/spiped -d -g -F -k \'/etc/spiped/redis.key\' -p /dev/null -s \'0.0.0.0:1234\' -t \'/var/run/redis.sock\'')
+          expect(content).to include('ExecStart=/usr/bin/spiped -d -g -F -k \'/etc/spiped/redis.key\' -s \'[0.0.0.0]:1234\' -t \'/var/run/redis.sock\'')
         end
       end
 
@@ -56,6 +58,105 @@ describe 'spiped::tunnel::server' do
           enable: true
         ).that_requires('Package[spiped]').that_subscribes_to('Systemd::Unit_file[spiped-redis.service]')
       }
+    end
+  end
+
+  describe 'conflicting parameters' do
+    describe 'source parameters' do
+      context 'source_socket_file and source_host' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            source_host: '0.0.0.0',
+            target_socket_file: '/var/run/redis.sock',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{`source_host` must not be specified when using `source_socket_file`}) }
+      end
+      context 'source_socket_file and source_port' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            source_port: 1234,
+            target_socket_file: '/var/run/redis.sock',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{`source_port` must not be specified when using `source_socket_file`}) }
+      end
+      context 'missing source_host' do
+        let(:params) do
+          {
+            source_port: 1234,
+            target_socket_file: '/var/run/redis.sock',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{either `source_socket_file` or `source_host` and `source_port must be specified}) }
+      end
+      context 'missing source_port' do
+        let(:params) do
+          {
+            source_host: '0.0.0.0',
+            target_socket_file: '/var/run/redis.sock',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{either `source_socket_file` or `source_host` and `source_port must be specified}) }
+      end
+    end
+    describe 'target parameters' do
+      context 'target_socket_file and target_host' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            target_socket_file: '/var/run/redis.sock',
+            target_host: 'redis-host',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{`target_host` must not be specified when using `target_socket_file`}) }
+      end
+      context 'target_socket_file and target_port' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            target_socket_file: '/var/run/redis.sock',
+            target_port: 1234,
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{`target_port` must not be specified when using `target_socket_file`}) }
+      end
+      context 'missing target_host' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            target_port: 1234,
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{either `target_socket_file` or `target_host` and `target_port must be specified}) }
+      end
+      context 'missing target_port' do
+        let(:params) do
+          {
+            source_socket_file: '/path/to/socket',
+            target_host: 'redis-host',
+            secret: 'hunter2'
+          }
+        end
+
+        it { is_expected.to compile.and_raise_error(%r{either `target_socket_file` or `target_host` and `target_port must be specified}) }
+      end
     end
   end
 end
